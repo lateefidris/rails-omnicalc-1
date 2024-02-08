8acb717e0f165374f36e0c5e2e2a704d8aa1c2cf
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2024-02-08 21:35:57 +0000

    App Working

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -4,16 +4,52 @@ class CalcController < ApplicationController
   end
 
   def square
-   
    render({:template => "templates/square"})
   end
 
   def square_results
-    @num = params.fetch("square").to_i
+    @num = params.fetch("square").to_f
     @square = @num * @num 
     render({:template => "templates/square_results"})
   end
 
   def square_root
+    render({:template => "templates/square_root"})
   end
+
+  def square_root_results
+    @num = params.fetch("square_root").to_f
+    @square_root = Math.sqrt(@num)
+    render({:template => "templates/square_root_results"})
+  end
+
+  def payment
+    render({:template => "templates/payment"})
+  end
+
+  def payment_results
+    @apr = params.fetch("apr").to_f
+    @yrs = params.fetch("yrs").to_f
+    @principal = params.fetch("principal").to_f
+  
+  
+    @apr = @apr /100 /12
+    @yrs *= 12
+  
+  
+    @payment = @principal * (@apr * (1 + @apr)** @yrs) / ((1 + @apr)** @yrs - 1)
+    render({:template => "templates/payment_results"})
+  end
+
+  def random
+    render({:template => "templates/random"})
+  end
+
+  def random_results
+    @min = params.fetch("min").to_f
+    @max = params.fetch("max").to_f
+
+    @random = rand(@min..@max)
+    render({:template => "templates/random_results"})
+  end 
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   get("/square/results", {:controller => "calc", :action => "square_results"})
 
   get("/square_root/new", {:controller => "calc", :action => "square_root"})
-  get("/square_root/results", {:controller => "calc", :action => "square_root"})
+  get("/square_root/results", {:controller => "calc", :action => "square_root_results"})
 
   get("/payment/new", {:controller => "calc", :action => "payment"})
-  get("/payment/results", {:controller => "calc", :action => "payment"})
+  get("/payment/results", {:controller => "calc", :action => "payment_results"})
 
   get("/random/new", {:controller => "calc", :action => "random"})
-  get("/random/results", {:controller => "calc", :action => "random"})
+  get("/random/results", {:controller => "calc", :action => "random_results"})
   
 end

4915761a614ab5b0ca2a87bca7e114a87b8aff1d
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2024-02-08 15:33:13 +0000

    Square & Square Result working

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -4,9 +4,14 @@ class CalcController < ApplicationController
   end
 
   def square
-   num = params.fetch("square")
-   @square = num 
+   
    render({:template => "templates/square"})
+  end
+
+  def square_results
+    @num = params.fetch("square").to_i
+    @square = @num * @num 
+    render({:template => "templates/square_results"})
   end
 
   def square_root
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get("/", {:controller => "calc", :action => "home"})
 
   get("/square/new", {:controller => "calc", :action => "square"})
-  get("/square/results", {:controller => "calc", :action => "square"})
+  get("/square/results", {:controller => "calc", :action => "square_results"})
 
   get("/square_root/new", {:controller => "calc", :action => "square_root"})
   get("/square_root/results", {:controller => "calc", :action => "square_root"})
